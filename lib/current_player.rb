f32086f12df548df2e4522a948d22fa81d963ee1
ed.a.ezekiel <ed.a.ezekiel@gmail.com>
2018-09-28 13:00:21 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,14 +4,10 @@ counter = 0
 def turn_count(board)
 
   board.each do | mark |
-    puts "The selection here is #{mark}."
-  end
-
+    if "#{mark}" == X
+      counter += 1
+    elsif "#{mark}" == O
+      counter += 1
+    end
+    end
 end
-
-
-#if mark == "X"
-#  counter += 1
-#elsif mark == "O"
-#  counter += 1
-#end
